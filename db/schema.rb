0964be25263611ea663d232f83e50973b57fa743
aperezsaez <alex.perez.22@live.com>
2018-09-26 20:39:38 -0300

    Removiendo Address de user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_233111) do
+ActiveRecord::Schema.define(version: 2018_09_26_233845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2018_09_26_233111) do
     t.string "name"
     t.string "photo"
     t.integer "phone"
-    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "bio"
