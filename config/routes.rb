2a8ef364c331e4ff6baf919c11525aecbbe172c7
aperezsaez <alex.perez.22@live.com>
2018-10-22 19:24:48 -0300

    Realizanco merge con rama master

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :pages
   devise_for :users, controllers: {
      sessions: 'users/sessions',
      registrations: 'users/registrations'
    }
 
-  root to: "pages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
