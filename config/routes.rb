cd60cbe1d48512abed1a6533d52efbccbe897385
aperezsaez <alex.perez.22@live.com>
2018-10-22 19:41:15 -0300

    Agregando inicio de sesión con facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
      sessions: 'users/sessions',
-     registrations: 'users/registrations'
+     registrations: 'users/registrations',
+     :omniauth_callbacks => "users/omniauth_callbacks"
    }
    resources :users do
      resources :appointments
    end
 
-
+ root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
