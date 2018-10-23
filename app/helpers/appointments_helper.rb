c12721f21d73c64e4889994269ab6ffbb5a83e76
aperezsaez <alex.perez.22@live.com>
2018-10-23 08:31:14 -0300

    Agregando FullCalendar

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,7 +1,2 @@
 module AppointmentsHelper
-  test 'cant create appointments with myself' do
-    i = Appointments.new(client:users(:one), professional:users(:one))
-    assert_not i.valid?, "cant interact with myself"
-  end
-
 end
