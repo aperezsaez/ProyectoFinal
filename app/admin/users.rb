45c40bbb59a208b4b238259a8310e1262b004782
aperezsaez <alex.perez.22@live.com>
2018-10-22 21:56:27 -0300

    Agregando appointments a activeadmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,33 +1,61 @@
+# frozen_string_literal: true
+
 ActiveAdmin.register User do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
 
-index do
-  column :name
-  column :id
-  column :email do |user|
-    user.email.split('@').first.capitalize
+  index do
+    column :name
+    column :id
+    column :email do |user|
+      user.email.split('@').first.capitalize
+    end
+    column :email_provider do |user|
+      user.email.split('@').last.capitalize
+    end
+    column :member_since do |user|
+      user.created_at.strftime('%F')
+    end
+    column :role
+    actions
   end
-  column :email_provider do |user|
-    user.email.split('@').last.capitalize
-  end
-  column :member_since do |user|
-    user.created_at.strftime('%F')
-  end
-  column :role
-  column :Ocupation do
-    user.ocupations.name
-  end
-end
 
+  form do |f|
+    inputs do
+      f.input :email
+      f.input :password
+      f.input :name
+      f.input :role
+      f.input :phone
+      f.input :photo
+      f.input :bio
+      actions
+    end
+  end
+
+  controller do
+    def update
+      if params[:user][:password].blank? && params[:user]
+        [:password_confirmation].blank?
+        params[:user].delete('password')
+        params[:user].delete('password_confirmation')
+      end
+      super
+    end
+  end
+
+  filter :role, as: :select
+  filter :email, as: :select
+
+  permit_params :email, :password, :photo, :role, :name, :bio, :phone
 end
