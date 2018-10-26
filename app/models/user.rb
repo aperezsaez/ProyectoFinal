d32c565ee9080813c8ebea85db2fc6ebaa4a4722
aperezsaez <alex.perez.22@live.com>
2018-10-26 13:12:27 -0300

    Merge con Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
   enum role: [:Admin, :Cliente, :Profesional]
   mount_uploader :photo, ImageUploader
   has_many :client_appointments, class_name: 'Appointment', foreign_key: :client_id, dependent: :destroy
