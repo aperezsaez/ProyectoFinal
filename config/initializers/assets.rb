d0fac71fd17165597b6b8f04c2b794e747dc7cfb
aperezsaez <alex.perez.22@live.com>
2018-11-07 22:39:35 -0300

    heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'images', 'slider')
+Rails.application.config.assets.paths << Rails.root.join('images', 'slider')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
